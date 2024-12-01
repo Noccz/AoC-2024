8207f73fe8c81f0ed5e5fcde3bcc1babcb497c25
Noccz <radiation_night@hotmail.com>
2024-12-01 12:05:18 +0100

    Add solution for day 1

---
+++ day1/day1.py
@@ -0,0 +1,42 @@
+import csv
+def part1(leftList, rightList):
+    distances = []
+
+    for i in range(len(leftList)):
+        distances.append(abs(leftList[i] - rightList[i]))
+
+    totalDistance = 0
+    for distance in distances:
+        totalDistance += distance
+
+    return totalDistance
+
+def part2(leftlist, rightList):
+    similarityScore = 0
+    f = open("output", "a")
+    for i in leftlist:
+        nrOfMatches = [index for index in range(len(rightList)) if rightList[index] == i]
+        similarityScore += i * len(nrOfMatches)
+
+    print(rightList)
+    return similarityScore
+
+
+def main():
+    leftList = []
+    rightList = []
+    with open("input", "r",) as input:
+        while line := input.readline():
+            lineList = line.rsplit()
+            leftList.append(int(lineList[0]))
+            rightList.append(int(lineList[1]))
+
+    leftList.sort()
+    rightList.sort()
+
+    print(part1(leftList, rightList))
+    print(part2(leftList, rightList))
+    
+
+if __name__=="__main__":
+    main()
